3ddfb50d257aa9ca13cad36616a17c58c0402d92
David Johnston <dwtj@dwtj.me>
2020-10-21 17:33:03 -0700

    Add `//test/generated_data/include_str` workaround

--- test/generated_data/include_str/include_str.rs
+++ test/generated_data/include_str/include_str.rs
@@ -6,7 +6,7 @@ mod test {
         let hello_world = format!(
             "{}, {}!",
             include_str!("source_data.txt").trim(),
-            include_str!("generated_data.txt").trim()
+            include_str!(concat!(env!("OUT_DIR"), "/../generated_data.txt")).trim()
         );
         println!("{}", hello_world);
         assert_eq!("Hello, world!", hello_world);
